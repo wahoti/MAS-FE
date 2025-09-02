bfcbfcec2f87bba2ba132ec3fdbdc8cd6529bea1
wahoti <abdul@firstfoundy.co>
2025-09-02 15:00:41 -0700

    use fetch instead of swr

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import React, { useEffect } from 'react';
 import './App.css';
-import useSWR from 'swr'
+// import useSWR from 'swr'
 
 const DEV = true;
 
 const API_URL = DEV ? 'http://localhost:3005' : '';
+
+const testEndpoint = `${API_URL}/test`;
 
 // const API_URL = process.env.API_URL;
 // const DEV = process.env.DEV;
@@ -11,13 +14,37 @@ const API_URL = DEV ? 'http://localhost:3005' : '';
 console.log('API_URL', API_URL);
 console.log('DEV', DEV);
 
+async function getData() {
+  const url = testEndpoint;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    // return response;
+    const result = await response.json();
+    return result;
+    // console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
 // const fetcher = (...args) => fetch(...args).then(res => res.json())
-const fetcher = (...args) => fetch(...args).then(res => res);
+// const fetcher = (...args) => fetch(...args).then(res => res);
 
 const App = () => {
-  const { data, error, isLoading } = useSWR(`${API_URL}/test`, fetcher)
+  // const { data, error, isLoading } = useSWR(`${API_URL}/test`)
+  // console.log(data, error, isLoading);
 
-  console.log(data, error, isLoading);
+  useEffect(() => {
+    const doFetchData = async () => {
+      const res = await getData();
+      console.log('res', res);
+    }
+    doFetchData();
+  }, []);
 
   return (
     <div className="content">
